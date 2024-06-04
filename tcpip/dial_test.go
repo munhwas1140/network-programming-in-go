4b8f082306c8b817cf5001620a8be5a0dbde1f73
Hyung Jun Noh <munhwas1140@naver.com>
2024-06-05 00:28:07 +0900

    2024-06-05

--- tcpip/dial_test.go
+++ tcpip/dial_test.go
@@ -7,7 +7,6 @@ import (
 )
 
 func TestDial(t *testing.T) {
-	// 랜덤 포트에 리스너 생성
 	listener, err := net.Listen("tcp", "127.0.0.1:")
 	if err != nil {
 		t.Fatal(err)
@@ -16,9 +15,15 @@ func TestDial(t *testing.T) {
 	done := make(chan struct{})
 	go func() {
 		defer func() { done <- struct{}{} }()
+
 		for {
+			// Accept() 메서드는 리스너가 수신 연결을 감지하고
+			// 클라이언트와 서버 간의 TCP 핸드셰이크 절차가 완료될 떄까지 블로킹된다.
 			conn, err := listener.Accept()
 			if err != nil {
+				// 57번 줄에서 listener가 Close 되면
+				// 블로킹이 해제되면서 에러를 반환한다.
+				// ex) accept tcp 127.0.0.1:50388: use of closed network connection
 				t.Log(err)
 				return
 			}
@@ -28,6 +33,7 @@ func TestDial(t *testing.T) {
 					c.Close()
 					done <- struct{}{}
 				}()
+
 				buf := make([]byte, 1024)
 				for {
 					n, err := c.Read(buf)
